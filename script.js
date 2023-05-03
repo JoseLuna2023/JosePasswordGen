86c621101cda53af7d99c5ac4906fbd0b8af73a7
JoseLuna2023 <130305124+JoseLuna2023@users.noreply.github.com>
2023-05-03 12:10:14 -0500

    getting length for password from user

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // .split("") === "ABC" => ["A", "B", "C"]
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')  
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
 var lower = "abcdefghijklmnopqrstuvwxyz".split('')
 var nums = "1234567890".split('')
 var symbol = "!@#$%^&*()_+".split('')
 
 function generatePassword() {
+    var notReady = []
+    var ready = ""
+    var passLength = prompt("How long would you like your password to be? (8-128)")
+    if(passLength > 128 || passLength < 8) {
+        alert("Youre password must be between 8 and 128. Please Retry!")
+        generatePassword()
+    }
+
 
 }
 
